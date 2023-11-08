68b1dffa9c12ba5b3ecfb6ffcf1a85713b5da7fb
justs <justs@WINDOW12>
2023-11-09 11:21:17 +1300

    first commit

---
+++ src/screens/CafeCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import IconButton from '@mui/material/IconButton';
+import CardMedia from '@mui/material/CardMedia';
+
+const CafeCard = () => {
+  return (
+    <Card sx={{ maxWidth: 600 }}>
+      ASasa
+    </Card>
+  );
+};
+
+export default CafeCard;
